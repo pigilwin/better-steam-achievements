b76dc6743c10cbbf97323e1badc3f6ea2214fb10
Tim Remnant <timremnant@icloud.com>
2023-08-02 19:32:26 +0100

    feat: adding credentials link

--- lib/achievements/features/preferences.dart
+++ lib/achievements/features/preferences.dart
@@ -1,8 +1,16 @@
 import 'package:better_steam_achievements/achievements/components/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class PreferencesPage extends StatelessWidget {
+class PreferencesPage extends StatefulWidget {
   const PreferencesPage({super.key});
+
+  @override
+  State<PreferencesPage> createState() => _PreferencesPage();
+}
+
+class _PreferencesPage extends State<PreferencesPage> {
+  final Map<int, bool> expanded = {0: false};
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +29,20 @@ class PreferencesPage extends StatelessWidget {
         ),
       ),
       drawer: const Menu(),
-      body: const Text("Preferences"),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.go('/config');
+              },
+              child: const Text("Change credentials"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
